5baa55c7b022f759c0f795243be36900d194fa8a
sahidularif <sahidularif1@gmail.com>
2022-11-14 03:35:40 +0600

    add services route

--- handler/service.handler.js
+++ handler/service.handler.js
@@ -1,14 +1,14 @@
-// const { Order } = require("../models/Order");
+const { Order } = require("../models/Order");
 
-// const serviceHandler = {}
-// serviceHandler.getAllOrder = async (req, res, next) => {
+const serviceHandler = {}
+serviceHandler.getAllOrder = async (req, res, next) => {
 
-//     try {
-//         const orders = await Order.find();
-//         res.status(200).send(orders);
-//       } catch (err) {
-//         res.status(500).json(err);
-//       }
+    try {
+        const orders = await Order.find();
+        res.status(200).send(orders);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 
-// }
-// module.exports = serviceHandler
+}
+module.exports = serviceHandler
--- routes/services.js
+++ routes/services.js
@@ -1,16 +1,9 @@
 const express = require('express')
-// const { getAllOrder } = require('../handler/service.handler')
+const { getAllOrder } = require('../handler/service.handler')
 const { isAdmin } = require('../handler/authHelper')
 const router = express.Router()
-const { Order } = require('../models/Order')
-router.get('/getAllOrder', isAdmin, async (req, res) => {
-    try {
-        const orders = await Order.find();
-        res.status(200).send(orders);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-})
+
+router.get('/getAllOrder', isAdmin, getAllOrder)
 
 
 
