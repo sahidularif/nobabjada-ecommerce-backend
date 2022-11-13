f7fc475bfe9e482e066a1dee9281c78d6174f33b
sahidularif <sahidularif1@gmail.com>
2022-11-14 01:58:29 +0600

    add routes and stripe

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 5000
-// const stripe = require('./routes/stripe');
-// const {routes } = require('./routes/auth.routes');
-// const { productRoute } = require('./routes/services');
+const stripe = require('./routes/stripe');
+const {routes } = require('./routes/auth.routes');
+const { productRoute } = require('./routes/services');
 require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
@@ -12,12 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Connect to  the Mongodb database
-// require('./database')();
+require('./database')();
 
 
-// app.use('/auth', routes);
-// app.use('/stripe', stripe);
-// app.use('/product', productRoute);
+app.use('/auth', routes);
+app.use('/stripe', stripe);
+app.use('/product', productRoute);
 
 app.get("/", (req, res) => {
     res.send({ message: "Welcome" })
