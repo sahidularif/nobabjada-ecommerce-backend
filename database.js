a192c4cfc5c6ae368b1a807d1ab439aafe881e16
sahidularif <sahidularif1@gmail.com>
2022-11-14 23:10:38 +0600

    stripe added

--- database.js
+++ database.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 // const DB = 'mongodb://localhost:27017/mydb'
+// const DB = 'mongodb://localhost:27017/mydb'
+
 const DB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.or4h7.mongodb.net/testDb?retryWrites=true&w=majority`
 
 module.exports = () => {
